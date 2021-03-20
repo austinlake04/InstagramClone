6b364602709ee1e8659da0c73d2ac40823447f1e
alake4 <austinlake04@gmail.com>
2021-03-20 16:44:24 -0700

    part 2 final commit fixed

--- app/src/main/java/com/example/instagramclone/PostsAdapter.java
+++ app/src/main/java/com/example/instagramclone/PostsAdapter.java
@@ -20,7 +20,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
     private Context context;
     private List<Post> posts;
 
-    public PostsAdapter(Context context, List<Post> allPosts) {
+    public PostsAdapter(Context context, List<Post> posts) {
         this.context = context;
         this.posts = posts;
     }
